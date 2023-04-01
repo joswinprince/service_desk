2bd6a1ab93ce8237bc8660585133712645a9007c
joswinprince <kingdomjoswin@gmail.com>
2023-04-01 09:08:36 +0530

    first commit

---
+++ service_desk/c2n___service_desk/doctype/support_ticket/support_ticket.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Hephzibah Technologies Inc and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Support Ticket', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ service_desk/c2n___service_desk/doctype/support_ticket/test_support_ticket.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Hephzibah Technologies Inc and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSupportTicket(FrappeTestCase):
+	pass
